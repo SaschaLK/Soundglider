5f8e43a0bfe07b751a28b40c4470ea73e4d8c512
SaschaLK <SaschaL.Kufahl@gmail.com>
2015-12-27 15:46:32 +0100

    up

---
+++ htmlCode/navigation.php
@@ -0,0 +1,13 @@
+<?php
+echo '
+<nav>
+	<table> 
+		<tr>
+			<td><a href=Main.html>Startseite</a></td> 
+			<td><a href=Bts.html>Behind the scenes</a></td> 
+			<td><a href=Sonstiges.html>Sonstiges</a></td>
+		</tr>
+	</table>
+</nav>
+'
+?>
